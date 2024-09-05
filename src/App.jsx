2b8e929195318e8def0715a2d0a5f2b34ca05008
J-sense <121811557+J-sense@users.noreply.github.com>
2024-09-05 08:34:20 +0600

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import './App.css'
-import HeroSection from './components/Herosection'
+import HeroSection from "./components/Herosection";
 import Navbar from './components/Navbar'
 import Latestmeal from './components/Latestmeal'
 import AvailableMeal from './components/AvailableMeal'
