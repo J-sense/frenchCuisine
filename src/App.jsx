5623f61237d2a9c66e75995ea0525d1aecc3ed54
J-sense <121811557+J-sense@users.noreply.github.com>
2024-09-05 08:27:31 +0600

    upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 import HeroSection from './cpmponents/Herosection'
 import Navbar from './cpmponents/Navbar'
@@ -8,7 +7,7 @@ import Latestmeal from './cpmponents/Latestmeal'
 import AvailableMeal from './cpmponents/AvailableMeal'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
    <>
